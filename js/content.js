debaf16681e8ad8030001d000c0f62d303753b26
fartman451 <zackmscott7@gmail.com>
2026-09-01 06:47:48 +0100

    Update content.js

--- js/content.js
+++ js/content.js
@@ -1,15 +1,15 @@
 import { round, score } from './score.js';
 
-const dir = '/GDListTemplate';
+const dir = 'data';
 
 export async function fetchList() {
-    const listResult = await fetch('/GDListTemplate/data/_list.json');
     try {
+        const listResult = await fetch(`${dir}/_list.json`);
         const list = await listResult.json();
         return await Promise.all(
             list.map(async (path, rank) => {
-                const levelResult = await fetch('/GDListTemplate/data/' + path + '.json');
                 try {
+                    const levelResult = await fetch(`${dir}/${path}.json`);
                     const level = await levelResult.json();
                     return [
                         {
@@ -106,7 +106,7 @@ export async function fetchLeaderboard() {
 
 export async function fetchEditors() {
     try {
-        const editorsResults = await fetch('/GDListTemplate/data/_editors.json');
+        const editorsResults = await fetch(`${dir}/_editors.json`);
         const editors = await editorsResults.json();
         return editors;
     } catch {
