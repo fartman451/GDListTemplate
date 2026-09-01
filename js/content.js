bebeff752c0eccd099f7dabf8b1cda8b9699793f
fartman451 <zackmscott7@gmail.com>
2026-09-01 07:41:00 +0100

    Update content.js

--- js/content.js
+++ js/content.js
@@ -2,12 +2,12 @@ import { round, score } from './score.js';
 
 export async function fetchList() {
     try {
-        const listResult = await fetch('/data/_list.json');
+        const listResult = await fetch('./data/_list.json');
         const list = await listResult.json();
         return await Promise.all(
             list.map(async (path, rank) => {
                 try {
-                    const levelResult = await fetch('/data/' + path + '.json');
+                    const levelResult = await fetch(`./data/${path}.json`);
                     const level = await levelResult.json();
                     return [
                         {
@@ -104,7 +104,7 @@ export async function fetchLeaderboard() {
 
 export async function fetchEditors() {
     try {
-        const editorsResults = await fetch('/data/_editors.json');
+        const editorsResults = await fetch('./data/_editors.json');
         const editors = await editorsResults.json();
         return editors;
     } catch {
