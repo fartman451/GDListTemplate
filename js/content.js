9b3897d8371822af487fd4d10541d03c388c3800
fartman451 <zackmscott7@gmail.com>
2026-09-01 07:01:42 +0100

    Update content.js

--- js/content.js
+++ js/content.js
@@ -1,16 +1,15 @@
 import { round, score } from './score.js';
 
-// Dynamically calculates the absolute root URL path of your specific repository
-const dir = `${window.location.origin}/${window.location.pathname.split('/')[1]}/data`;
+const dir = './data';
 
 export async function fetchList() {
     try {
-        const listResult = await fetch(`${dir}/_list.json`);
+        const listResult = await fetch('./data/_list.json');
         const list = await listResult.json();
         return await Promise.all(
             list.map(async (path, rank) => {
                 try {
-                    const levelResult = await fetch(`${dir}/${path}.json`);
+                    const levelResult = await fetch('./data/' + path + '.json');
                     const level = await levelResult.json();
                     return [
                         {
@@ -107,7 +106,7 @@ export async function fetchLeaderboard() {
 
 export async function fetchEditors() {
     try {
-        const editorsResults = await fetch(`${dir}/_editors.json`);
+        const editorsResults = await fetch('./data/_editors.json');
         const editors = await editorsResults.json();
         return editors;
     } catch {
