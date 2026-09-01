5f79f46217d4acbdbf95524197079106a40354db
fartman451 <zackmscott7@gmail.com>
2026-09-01 07:52:56 +0100

    Update content.js

--- js/content.js
+++ js/content.js
@@ -1,13 +1,16 @@
 import { round, score } from './score.js';
 
+// Generates a random number string to bypass server caching
+const bust = `?cache=${Date.now()}`;
+
 export async function fetchList() {
     try {
-        const listResult = await fetch('./data/_list.json');
+        const listResult = await fetch(`./data/_list.json${bust}`);
         const list = await listResult.json();
         return await Promise.all(
             list.map(async (path, rank) => {
                 try {
-                    const levelResult = await fetch(`./data/${path}.json`);
+                    const levelResult = await fetch(`./data/${path}.json${bust}`);
                     const level = await levelResult.json();
                     return [
                         {
@@ -104,7 +107,7 @@ export async function fetchLeaderboard() {
 
 export async function fetchEditors() {
     try {
-        const editorsResults = await fetch('./data/_editors.json');
+        const editorsResults = await fetch(`./data/_editors.json${bust}`);
         const editors = await editorsResults.json();
         return editors;
     } catch {
