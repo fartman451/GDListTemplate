ea44692da990eac778fe790ddc5b329a4173b1a3
fartman451 <zackmscott7@gmail.com>
2026-09-01 08:06:30 +0100

    Update content.js

--- js/content.js
+++ js/content.js
@@ -4,14 +4,13 @@ const bust = `?cache=${Date.now()}`;
 
 export async function fetchList() {
     try {
-        const listResult = await fetch(`./data/_list.json${bust}`);
+        const listResult = await fetch(`/data/_list.json${bust}`);
         const list = await listResult.json();
         return await Promise.all(
             list.map(async (path, rank) => {
                 try {
-                    const levelResult = await fetch(`./data/${path}.json${bust}`);
+                    const levelResult = await fetch(`/data/${path}.json${bust}`);
                     const level = await levelResult.json();
-                    // Returns an inner array element to satisfy the template framework
                     return [
                         {
                             ...level,
@@ -30,7 +29,6 @@ export async function fetchList() {
         );
     } catch {
         console.error('Failed to load list.');
-        // Must return an empty array instead of null to prevent component crashes
         return [];
     }
 }
@@ -108,7 +106,7 @@ export async function fetchLeaderboard() {
 
 export async function fetchEditors() {
     try {
-        const editorsResults = await fetch(`./data/_editors.json${bust}`);
+        const editorsResults = await fetch(`/data/_editors.json${bust}`);
         const editors = await editorsResults.json();
         return editors;
     } catch {
